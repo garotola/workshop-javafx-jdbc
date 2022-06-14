7132613f65e84c2a3a82f2b99c8c5ca2fad53c9e
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-06-14 17:11:03 -0300

    Criação da Interface e Lógica da Aba sobre

--- src/app/App.java
+++ src/app/App.java
@@ -1,3 +1,4 @@
+package app;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,6 +7,12 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class App extends Application{
+
+    public static Scene getMainScene(){
+        return mainScene;
+    }
+
+    private static Scene mainScene;
     public static void main(String[] args) {
         launch(args);
     }
@@ -16,8 +23,8 @@ public class App extends Application{
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
             scrollPane.setFitToHeight(true); scrollPane.setFitToWidth(true); // Ocupar todo o espaço que lhe derem
-            Scene mainScene = new Scene(scrollPane);
-            stage.setTitle("Simple JavaFx Applicatin");
+            mainScene = new Scene(scrollPane);
+            stage.setTitle("Gestão de Vendendores e Departamentos");
             stage.setScene(mainScene);
             stage.show();
         }catch(IOException e){
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import app.App;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -23,13 +32,29 @@ public class MainViewController implements Initializable {
     }
 
     public void onMenuItemAboutAction(){
-        System.out.println("Aqui foi!");
+        loadView("/gui/About.fxml");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
         
+    }
+
+    private void loadView(String absoluteName){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+            Scene mainScene = App.getMainScene();
+            VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+            Node mainMenu = mainVbox.getChildren().get(0);
+            mainVbox.getChildren().clear();
+            mainVbox.getChildren().add(mainMenu);
+            mainVbox.getChildren().addAll(newVBox.getChildren());
+
+        }catch(IOException e){
+            Alerts.showAlert("IO Exception", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+        }
     }
     
 }
